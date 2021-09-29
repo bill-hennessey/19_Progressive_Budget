8da7389a372ed09ac81b1c94f6ff60971892047b
bill-hennessey <bhennessey1020@gmail.com>
2021-09-29 17:50:21 -0600

    tweaking db functions

--- public/db.js
+++ public/db.js
@@ -10,7 +10,7 @@ request.onupgradeneeded = ({ target }) => {
     checkDatabase();
   }
 
-  const objectstore = db.createObjectStore("budget", { keypath: "__id" });
+  const objectstore = db.createObjectStore("budget", { keypath: "_id" });
   objectstore.createIndex("value", "value");
 };
 
@@ -23,39 +23,4 @@ request.onsuccess = (event) => {
   budgetStore.add({});
 };
 
-// function saveRecord(record) {
-//   // create a transaction on the pending db with readwrite access
-//   const transaction = db.transaction(["budget"], "readwrite");
-//   // access your pending object store
-//   const budgetStore = transaction.objectStore("budget");
-//   // add record to your store with add method.
-//   budgetStore.add({name: , value: populateTotal});
-// }
-
-// function checkDatabase() {
-//   // open a transaction on your pending db
-//   // access your pending object store
-//   // get all records from store and set to a variable
-
-//   getAll.onsuccess = function () {
-//     if (getAll.result.length > 0) {
-//       fetch("/api/transaction/bulk", {
-//         method: "POST",
-//         body: JSON.stringify(getAll.result),
-//         headers: {
-//           Accept: "application/json, text/plain, */*",
-//           "Content-Type": "application/json",
-//         },
-//       })
-//         .then((response) => response.json())
-//         .then(() => {
-//           // if successful, open a transaction on your pending db
-//           // access your pending object store
-//           // clear all items in your store
-//         });
-//     }
-//   };
-// }
-
-// listen for app coming back online
 window.addEventListener("online", checkDatabase);
